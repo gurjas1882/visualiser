022b07c2b0d24117e53ab43dfc9a1f2c60515b93
gurjas1882 <gurjas1882@gmail.com>
2023-09-16 12:19:47 -0400

    Periodic Safety Commit 1

--- src/App.js
+++ src/App.js
@@ -9,7 +9,10 @@ function App() {
 	const redirect = "http://localhost:3000";
 	const scope = "user-top-read";
 
-	const [token, setToken] = useState(0);
+	const [token, setToken] = useState("");
+	const [selectedOptions, setSelectedOption] = useState("");
+	const [artistsData, setArtistsData] = useState([]);
+	const [genreCount, setGenreCount] = useState({});
 
 	useEffect(() => {
 		const hash = window.location.hash;
@@ -27,22 +30,63 @@ function App() {
 		setToken(access_token);
 	}, []);
 
-	async function getArtists() {
-		const {data} = await axios.get("https://api.spotify.com/v1/me/top/artists?limit=50&offset=0", {
+	const getArtists = async () => {
+		const {data} = await axios.get(`https://api.spotify.com/v1/me/top/artists?limit=50&offset=0`, {
 			headers: {
 				Authorization: `Bearer ${token}`,
 			},
 		});
+		console.log(data);
+		let genreObject = {};
 		for (const item of data.items) {
-			console.log(item.name, item.genres);
+			console.log(item);
+			item.genres.forEach((genre) => {
+				if (!genreObject[genre]) {
+					genreObject[genre] = 1;
+				} else {
+					console.log("dupe");
+					genreObject[genre] = genreObject[genre] + 1;
+				}
+			});
 		}
-	}
+
+		setArtistsData(data.items);
+		setGenreCount(genreObject);
+	};
+
+	const handleOptionChange = (event) => {
+		setSelectedOption(event.target.value);
+		console.log(selectedOptions);
+		getArtists();
+	};
+
+	const renderArtists = () => {
+		return (
+			<div className="artistsContainer">
+				{artistsData.map((a) => (
+					<div key={a.id}>
+						<img src={a.images[0].url} />
+						{a.name}
+					</div>
+				))}
+				<div>
+					{Object.entries(genreCount).map(([key, value]) => (
+						<div key={key}>
+							{key}: {value}
+						</div>
+					))}
+				</div>
+			</div>
+		);
+	};
 
 	return (
 		<div className="App">
 			<header className="App-header">
-				<a href={`${endpoint}?client_id=${client_id}&redirect_uri=${redirect}&response_type=token&scope=${scope}`}>log in</a>
-				<button onClick={getArtists}>wad</button>
+				{!token ? <a href={`${endpoint}?client_id=${client_id}&redirect_uri=${redirect}&response_type=token&scope=${scope}`}>log in</a> : ""}
+
+				<button onClick={getArtists}>start</button>
+				{renderArtists()}
 			</header>
 		</div>
 	);
