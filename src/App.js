1722989a51cc59808c1090e3a70c527e637673d9
gurjas1882 <gurjas1882@gmail.com>
2023-09-16 16:18:15 -0400

    Safety Commit 2

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import logo from "./logo.svg";
 import "./App.css";
-import {useEffect, useState} from "react";
+import {useEffect, useRef, useState} from "react";
 import axios from "axios";
+import * as d3 from "d3";
+
+import {MapContainer, TileLayer, useMap, Marker, Popup, FeatureGroup, Circle, Tooltip, SVGOverlay} from "react-leaflet";
+import "leaflet/dist/leaflet.css";
 
 function App() {
 	const endpoint = "https://accounts.spotify.com/authorize";
@@ -61,33 +65,86 @@ function App() {
 	};
 
 	const renderArtists = () => {
+		let i = 0;
 		return (
-			<div className="artistsContainer">
-				{artistsData.map((a) => (
-					<div key={a.id}>
-						<img src={a.images[0].url} />
-						{a.name}
-					</div>
-				))}
-				<div>
+			<div className="artistWrap">
+				<div className="artistsHeader">
+					<h1>TOP ARTISTS</h1>
+					{/* <button className="track" /> */}
+				</div>
+				<div className="artistsContainer">
+					{artistsData.map((a, i) => (
+						<div key={a.id}>
+							<h6>{i + 1}.</h6>
+							<img src={a.images[0].url} />
+							<h6>{a.name}</h6>
+						</div>
+					))}
+					{/* <div>
 					{Object.entries(genreCount).map(([key, value]) => (
 						<div key={key}>
 							{key}: {value}
 						</div>
 					))}
+				</div> */}
 				</div>
+			</div>
+		);
+	};
+
+	const generateMap = () => {
+		const preProssessed = Object.entries(genreCount).map(([key, value]) => ({
+			genre: key,
+			radius: value / 8,
+			randomY: Math.floor(Math.random() * (40 - 60) + 60),
+			randomX: Math.floor(Math.random() * (40 - 60) + 60),
+		}));
+
+		const svgRef = useRef(null);
+
+		useEffect(() => {
+			const width = 400;
+			const height = 400;
+		});
+
+		return (
+			<div className="map">
+				<MapContainer center={[51.505, -0.52]} zoom={17} scrollWheelZoom={true} wheelPxPerZoomLevel={200}>
+					<TileLayer attribution="" url="" />
+					<SVGOverlay
+						attributes={{stroke: "red"}}
+						bounds={[
+							[51.4, -1],
+							[51.61, -0.04],
+						]}
+					>
+						<rect x="0" y="0" width="100%" height="100%" fill="black" />
+
+						{preProcessed.map((key, index) => (
+							<g key={index}>
+								<circle r={`${key.radius}%`} cx={`${key.randomX}%`} cy={`${key.randomY}%`} fill="red" />
+								<text x={`${key.randomX}%`} y={`${key.randomY}%`} fontSize="15" text-anchor="middle" alignment-baseline="middle" stroke="white">
+									{key.genre}
+								</text>
+							</g>
+						))}
+					</SVGOverlay>
+				</MapContainer>
 			</div>
 		);
 	};
 
 	return (
 		<div className="App">
-			<header className="App-header">
-				{!token ? <a href={`${endpoint}?client_id=${client_id}&redirect_uri=${redirect}&response_type=token&scope=${scope}`}>log in</a> : ""}
+			<div className="App-container">
+				<header className="App-header">
+					{!token ? <a href={`${endpoint}?client_id=${client_id}&redirect_uri=${redirect}&response_type=token&scope=${scope}`}>log in</a> : ""}
 
-				<button onClick={getArtists}>start</button>
-				{renderArtists()}
-			</header>
+					{artistsData.length === 0 ? <button onClick={getArtists}>start</button> : ""}
+					{renderArtists()}
+				</header>
+				{generateMap()}
+			</div>
 		</div>
 	);
 }
